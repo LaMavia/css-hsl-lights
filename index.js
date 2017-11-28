33a4ae2ae428aedf094e43dcd8020e23c3cba80a
LastShadowPL <xeloxpl@windowslive.com>
2017-11-28 23:32:45 +0100

    added new lights

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 "use strict";
+const random = (max, min = 1) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
 const sliders = document.querySelectorAll('.input');
 const state = {
     h: 0,
     s: 100,
     l: 50
 };
+(() => {
+    for (let i = 0; i < 20; i++) {
+        let el = document.createElement('div');
+        el.classList.add('light');
+        el.style.setProperty('--x', `${random(window.innerHeight, 0)}px`);
+        el.style.setProperty('--y', `${random(window.innerHeight, 0)}px`);
+        document.body.appendChild(el);
+    }
+})();
 const handleSlide = (e) => {
     let id = e.target.id, value = e.target.value, styleVal = id !== 'h' ? `${value}%` : value;
     state[id] = value;
--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
+const random = (max:any,min = 1) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
 const sliders = document.querySelectorAll('.input')
 const state = {
   h: 0,
   s: 100,
   l: 50
 } as any
+(() => {
+  for(let i = 0; i < 20; i++){
+    let el = document.createElement('div')
+    el.classList.add('light')
+    el.style.setProperty('--x', `${random(window.innerHeight,0)}px`)
+    el.style.setProperty('--y', `${random(window.innerHeight,0)}px`)
+    document.body.appendChild(el)
+  }
+})()
 const handleSlide = (e:any) => {
   let id:string = e.target.id,
       value = e.target.value,
