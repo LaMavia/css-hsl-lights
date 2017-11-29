bab9fc3c3f3d2c959d9d20f823a42cc7e8c335ff
LastShadowPL <xeloxpl@windowslive.com>
2017-11-29 19:37:41 +0100

    It's alive!! \n Now give me coffee

--- index.ts
+++ index.ts
@@ -1,20 +1,53 @@
-const random = (max:any,min = 1) => {
+const random = (max:number,min = 1) => {
   return Math.floor(Math.random() * (max - min) + min)
 }
-const sliders = document.querySelectorAll('.input')
+
+class Light{
+  x:number
+  y:number
+  dx:number
+  dy:number
+  el:HTMLElement
+  constructor(){
+    this.x = random(100,0)
+    this.y = random(100,0)
+    this.dx = 1
+    this.dy = 1
+    this.el
+  }
+
+  init(){
+    let el = document.createElement('div')
+    el.classList.add('light')
+    this.el = el
+    document.body.appendChild(el)
+    el.style.setProperty('--x', `${this.x}vw`)
+    el.style.setProperty('--y', `${this.y}vh`)
+    el.style.setProperty('--rand', `${Math.random()}`)
+  }
+  update(){
+    if(this.x < 0 || this.x > 100) this.dx *= -1
+    if(this.y < 0 || this.y > 100) this.dy *= -1
+    this.x += this.dx
+    this.y += this.dy
+  }
+  draw(){
+    this.el.style.setProperty('--x',`${this.x}vw`)
+    this.el.style.setProperty('--y',`${this.y}vh`)
+  }
+}
+const lights:Light[] = []
 const state = {
   h: 0,
   s: 100,
   l: 50
 } as any
 (() => {
-  for(let i = 0; i < 20; i++){
-    let el = document.createElement('div')
-    el.classList.add('light')
-    el.style.setProperty('--x', `${random(100,0)}%`)
-    el.style.setProperty('--y', `${random(100,0)}%`)
-    el.style.setProperty('--rand', `${Math.random()}`)
-    document.body.appendChild(el)
+  const slowdown = 20
+  for(let i = 0; i < 100; i++){
+    let newLight = new Light()
+    newLight.init()
+    lights.push(newLight)
   }
 })()
 const handleSlide = (e:any) => {
@@ -24,7 +57,17 @@ const handleSlide = (e:any) => {
   state[id] = value
   document.body.style.setProperty(`--${id}`, styleVal)
 }
-
+const sliders = document.querySelectorAll('.input')
 sliders.forEach(slider => {
-  slider.addEventListener('change', handleSlide,)
+  slider.addEventListener('change', handleSlide)
 })
+
+
+const animate = () => {
+  requestAnimationFrame(animate)
+  lights.forEach(light => {
+    light.update()
+    light.draw()
+  })
+}
+requestAnimationFrame(animate)
